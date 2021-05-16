c1822f63184ff23e951c8ab93cf35bbf831bcc30
Ananda Bhaumik <bhaumikananda@gmail.com>
2021-05-16 12:59:18 +0530

    Assignment

--- src/main/java/udemy/java/eleven/VariableAssignment.java
+++ src/main/java/udemy/java/eleven/VariableAssignment.java
@@ -6,14 +6,14 @@ public class VariableAssignment {
 		int primitiveCats = 10;
 		int primitiveDogs = primitiveCats;
 		primitiveCats = 14;
-		System.out.println("Count of primitiveCat is "+primitiveCats);
-		System.out.println("Count of primitiveDog is "+primitiveDogs);		
+		System.out.println("Count of primitiveCat is "+ primitiveCats);
+		System.out.println("Count of primitiveDog is "+ primitiveDogs);		
 		
 		Integer wrapCats = 10;
 		Integer wrapDogs = wrapCats;
 		wrapCats = 14;
-		System.out.println("Count of wrapCat is "+wrapCats);
-		System.out.println("Count of wrapeDog is "+wrapDogs);		
+		System.out.println("Count of wrapCat is "+ wrapCats);
+		System.out.println("Count of wrapeDog is "+ wrapDogs);		
 		
 	}
 
