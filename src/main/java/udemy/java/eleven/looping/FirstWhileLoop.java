41fb13555970fac1859ceda6184fb4cccc028569
Ananda Bhaumik <bhaumikananda@gmail.com>
2021-06-06 22:34:20 +0530

    Update FirstWhileLoop.java

--- src/main/java/udemy/java/eleven/looping/FirstWhileLoop.java
+++ src/main/java/udemy/java/eleven/looping/FirstWhileLoop.java
@@ -5,7 +5,7 @@ public class FirstWhileLoop {
 	public static void main(String[] args) {
 		System.out.printf("%s is a good boy, His rank is currently %d \n", "Ananda", 5);
 		double firstDouble = 4.1;
-		double secondDouble = 4.2;
+		double secondDouble = 4.1;
 		boolean testResult = firstDouble==secondDouble;
 		System.out.println("What is the result of " + testResult);
 	}
