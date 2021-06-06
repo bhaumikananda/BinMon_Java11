fbdc300911b6454f98506068815b0a10952f2c54
Ananda Bhaumik <bhaumikananda@gmail.com>
2021-06-06 22:22:03 +0530

    Double equaliyy check
    Double check does not work. Trying to see that .

---
+++ src/main/java/udemy/java/eleven/looping/FirstWhileLoop.java
@@ -0,0 +1,13 @@
+package udemy.java.eleven.looping;
+
+public class FirstWhileLoop {
+	
+	public static void main(String[] args) {
+		System.out.printf("%s is a good boy, His rank is currently %d \n", "Ananda", 5);
+		double firstDouble = 4.1;
+		double secondDouble = 4.2;
+		boolean testResult = firstDouble==secondDouble;
+		System.out.println("What is the result of " + testResult);
+	}
+
+}
