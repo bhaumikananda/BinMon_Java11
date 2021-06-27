9a78c0967c113624d632126f9c78b49024c8e0f2
Ananda Bhaumik <bhaumikananda@gmail.com>
2021-06-27 16:27:39 +0530

    Create a FInal class
    This is test final class

---
+++ src/main/java/udemy/java/eleven/loopsNcondition/TestFinalClass.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package udemy.java.eleven.loopsNcondition;
+
+import udemy.java.eleven.loopsNcondition.TestFinal.FinalClass;
+
+/**
+ * @author BinMon
+ *
+ */
+public class TestFinalClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		FinalClass fc = new FinalClass(5);
+		fc.print();
+		
+		
+	}
+
+}
